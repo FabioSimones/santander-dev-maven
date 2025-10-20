586171a7772f8aad5a3bd374a4e5dc3cc0c71240
FabioSimones <simones.fabio1994@gmail.com>
2025-10-20 18:09:49 -0300

    Implementação da camada de domínio.

---
+++ src/main/java/dev/fabiosimones/santander_dev/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.fabiosimones.santander_dev.domain.repository;
+
+import dev.fabiosimones.santander_dev.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
